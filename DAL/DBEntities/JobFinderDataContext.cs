ba6cae775c1d44968150395dbd25c4b735926e16
Asy1921 <amitabh.suyash@gmail.com>
2024-01-27 22:21:58 +0530

    Setting up migrations

--- DAL/DBEntities/JobFinderDataContext.cs
+++ DAL/DBEntities/JobFinderDataContext.cs
@@ -3,13 +3,22 @@ using Microsoft.EntityFrameworkCore;
 namespace DAL;
 public class JobFinderDataContext : DbContext
 {
-    public JobFinderDataContext(DbContextOptions<JobFinderDataContext> options) : base(options)
-    { }
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    public JobFinderDataContext()
     {
-        if (!options.IsConfigured)
+    }
+
+    public JobFinderDataContext(DbContextOptions<JobFinderDataContext> opt) : base(opt)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
         {
-            options.UseSqlServer(ConnStr.Get());
+
+            optionsBuilder.UseSqlServer(ConnStr.Get());
+
         }
     }
     public DbSet<tbl_Avl_Jobs> tbl_Avl_Jobs { get; set; }
---
+++ DAL/Migrations/20240127164948_TestDB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbl_Avl_Jobs",
+                columns: table => new
+                {
+                    Job_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Job_Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_Avl_Jobs", x => x.Job_ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbl_Avl_Jobs");
+        }
+    }
+}
