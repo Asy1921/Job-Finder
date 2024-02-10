0c33c36d2aea1a4703df62a7f4ce3747fcd21011
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2024-02-10 14:40:13 +0530

    Company Table Added

--- DAL/DBEntities/tbl_Avl_Jobs.cs
+++ DAL/DBEntities/tbl_Avl_Jobs.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL;
 
@@ -8,9 +9,13 @@ public class tbl_Avl_Jobs
     public string Job_ID { get; set; }
     public string CreatorUserID { get; set; }
     public string Job_Name { get; set; }
+    public string Coy_ID { get; set; }
     public int AvailablePositions { get; set; }
     public int FilledPositions { get; set; }
     public bool JobOpen { get; set; }
     public string Domain { get; set; }
     public string SkillsRequired { get; set; }
+
+    [ForeignKey("Coy_ID")]
+    public tbl_Companies Company { get; set; }
 }
