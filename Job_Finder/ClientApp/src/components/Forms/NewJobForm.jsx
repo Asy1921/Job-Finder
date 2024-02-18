d30d5dbc7d7d8fa78c7d49a17448297abe798fa7
Asy1921 <amitabh.suyash@gmail.com>
2024-02-18 15:48:53 +0530

    WIP - Input Form

--- Job_Finder/ClientApp/src/components/Forms/NewJobForm.jsx
+++ Job_Finder/ClientApp/src/components/Forms/NewJobForm.jsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "./jobForm.css";
 
 const JobForm = () => {
+  const [qualifications, setQualifications] = useState([
+    { name: "", required: "Optional" },
+  ]);
+  const [skills, setSkills] = useState([{ name: "", required: "Optional" }]);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const handleAddSkill = () => {
+    setSkills([...skills, { name: "", required: "Optional" }]);
+  };
+
+  const handleSkillChange = (index, event) => {
+    const { value } = event.target;
+    const updatedSkills = [...skills];
+    updatedSkills[index].name = value;
+    setSkills(updatedSkills);
+  };
+
+  const handleRemoveSkill = (index) => {
+    const updatedSkills = [...skills];
+    updatedSkills.splice(index, 1);
+    setSkills(updatedSkills);
+  };
+  const handleAddQualification = () => {
+    setQualifications([...qualifications, { name: "", required: "Optional" }]);
+  };
+
+  const handleQualificationChange = (index, event) => {
+    const { value } = event.target;
+    const updatedQualifications = [...qualifications];
+    updatedQualifications[index].name = value;
+
+    setQualifications(updatedQualifications);
+  };
+
+  const handleRemoveQualification = (index) => {
+    const updatedQualifications = [...qualifications];
+    updatedQualifications.splice(index, 1);
+    setQualifications(updatedQualifications);
+  };
 
   const onSubmit = (data) => {
+    // Include qualifications and skills in the form data
+    data.qualifications = qualifications;
+    data.skills = skills;
     console.log(data);
     // Submit logic here
   };
@@ -39,14 +80,6 @@ const JobForm = () => {
           <option value="No">No</option>
         </select>
       </div>
-
-      {/* <div className="form-group">
-        <label>Company ID</label>
-        <input {...register("companyId", { required: true })} />
-        {errors.companyId && (
-          <span className="error-message">This field is required</span>
-        )}
-      </div> */}
 
       <div className="form-group">
         <label>Available Opening</label>
@@ -91,21 +124,64 @@ const JobForm = () => {
       </div>
 
       <div className="form-group">
-        <label>Qualification required</label>
-        <select multiple {...register("qualificationRequired")}>
-          <option value="Bachelor">Bachelor</option>
-          <option value="Master">Master</option>
-          <option value="PhD">PhD</option>
-        </select>
+        <label>Qualifications required</label>
+        {qualifications.map((qualification, index) => (
+          <div key={index} className="multi-row">
+            <input
+              type="text"
+              value={qualification.name}
+              onChange={(e) => handleQualificationChange(index, e)}
+              name={`qualifications[${index}].name`}
+              placeholder="Qualification"
+            />
+            <select
+              value={qualification.required}
+              onChange={(e) => handleQualificationChange(index, e)}
+              name="required"
+            >
+              <option value="Optional">Optional</option>
+              <option value="Mandatory">Mandatory</option>
+            </select>
+            <button
+              type="button"
+              onClick={() => handleRemoveQualification(index)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+        <button type="button" onClick={handleAddQualification}>
+          Add New Qualification
+        </button>
       </div>
 
       <div className="form-group">
         <label>Skills required</label>
-        <select multiple {...register("skillsRequired")}>
-          <option value="JavaScript">JavaScript</option>
-          <option value="React">React</option>
-          <option value="Node.js">Node.js</option>
-        </select>
+        {skills.map((skill, index) => (
+          <div key={index} className="multi-row">
+            <input
+              type="text"
+              value={skill.name}
+              onChange={(e) => handleSkillChange(index, e)}
+              name={`skills[${index}].name`}
+              placeholder="Skill"
+            />
+            <select
+              value={skill.required}
+              onChange={(e) => handleSkillChange(index, e)}
+              name={`skills[${index}].required`}
+            >
+              <option value="Optional">Optional</option>
+              <option value="Mandatory">Mandatory</option>
+            </select>
+            <button type="button" onClick={() => handleRemoveSkill(index)}>
+              Remove
+            </button>
+          </div>
+        ))}
+        <button type="button" onClick={handleAddSkill}>
+          Add New Skill
+        </button>
       </div>
 
       <button type="submit">Submit</button>
