6d730980a3d336d49a5a2a7a0437cea1c1ef19cd
Asy1921 <amitabh.suyash@gmail.com>
2024-03-12 01:37:06 +0530

    user form path css

--- Job_Finder/ClientApp/src/components/Forms/UserForm.jsx
+++ Job_Finder/ClientApp/src/components/Forms/UserForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import "./userForm.css  ";
+import "./userForm.css";
 
 const initialValues = {
   userId: "",
